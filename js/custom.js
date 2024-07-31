26ca066b45e39f655e585e7d49537fc8f0c79a3e
Nahuel-Beck <juannahuelbeck@gmail.com>
2024-07-31 00:55:18 -0300

    Correcciones en form Netlify

--- js/custom.js
+++ js/custom.js
@@ -27,3 +27,16 @@ inputs.forEach(function(input) {
     btnDisabled.disabled = !(allFilled && recaptchaValidated);
   });
 });
+
+// Función de callback para reCAPTCHA
+function recaptchaCallback() {
+  recaptchaValidated = true;
+  // Verifica si todos los campos están llenos para habilitar el botón
+  var allFilled = true;
+  inputs.forEach(function(input) {
+    if (input.value.trim() === '') {
+      allFilled = false;
+    }
+  });
+  btnDisabled.disabled = !(allFilled && recaptchaValidated);
+}
