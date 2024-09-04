0ef3328f90a4031ed94a623bf5d5de6eb621ed23
Nahuel-Beck <juannahuelbeck@gmail.com>
2024-09-04 00:04:48 -0300

    se quita validacion de captcha

--- js/custom.js
+++ js/custom.js
@@ -8,35 +8,22 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 var formulario = document.getElementById('form-contacto');
 var btnDisabled = document.getElementById('btn-form');
 
-// Variable para rastrear el estado de reCAPTCHA
-var recaptchaValidated = false;
-
 // Selecciona todos los elementos de entrada y textarea del formulario
 var inputs = formulario.querySelectorAll('input, textarea');
 
 // Función para verificar si todos los campos están llenos
 function checkFields() {
   var allFilled = true;
-  
-  // Revisa si todos los campos de input y textarea tienen algún valor
   inputs.forEach(function(input) {
     if (input.value.trim() === '') {
       allFilled = false;
     }
   });
-
-  // Habilita o deshabilita el botón de envío solo si todos los campos están llenos y reCAPTCHA está validado
-  btnDisabled.disabled = !(allFilled && recaptchaValidated);
+  // Habilita o deshabilita el botón basado en si todos los campos están llenos
+  btnDisabled.disabled = !allFilled;
 }
 
-// Añade un evento de entrada a todos los elementos de input y textarea del formulario
+// Añade un evento de entrada a todos los elementos de entrada y textarea del formulario
 inputs.forEach(function(input) {
   input.addEventListener('input', checkFields);
 });
-
-// Función de callback para reCAPTCHA
-function recaptchaCallback() {
-  recaptchaValidated = true;
-  checkFields(); // Revisa los campos nuevamente cuando el reCAPTCHA se valida
-}
-
